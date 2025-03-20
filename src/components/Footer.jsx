87ee1b4a012c67a18dec9c2bb1f5a10faa69c1b2
Nischal <nischalbasnet7848@gmail.com>
2025-03-20 09:13:53 +0545

    footer changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 
 export default function Footer() {
   return (
-    <footer className='relative'>
-      <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 1440 320'>
+    <footer className='relative w-full h-[250px] mt-10'>
+      {/* using svg */}
+      {/* <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 1440 320'>
         <path
           fill='#000000'
           fillOpacity='1'
@@ -12,6 +13,14 @@ export default function Footer() {
       </svg>
       <div className='absolute bottom-0 inset-x-0 top-[50%] flex flex-col justify-center items-center text-white'>
         <h1 className='text-xl font-bold'>Develop By</h1>
+        <h1 className='text-lg'>Some One Anonymous</h1>
+      </div> */}
+      <div
+        className='absolute top-0 left-0 h-full w-full bg-black'
+        style={{ clipPath: 'polygon(0% 50%, 100% 0%, 100% 100%, 0% 100%)' }}
+      ></div>
+      <div className='absolute text-white inset-x-0 bottom-0 top-[50%] text-center flex flex-col items-center justify-center'>
+        <h1 className='text-xl font-bold'>Developed By</h1>
         <h1 className='text-lg'>Some One Anonymous</h1>
       </div>
     </footer>
